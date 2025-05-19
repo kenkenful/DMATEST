9533f4c9117bddd8aee1fd0b93b702892ec39386
kenkenful <t0127mikaken@gmail.com>
2025-05-20 06:55:41 +0900

    Update dma.cpp

--- dma.cpp
+++ dma.cpp
@@ -1,25 +1,12 @@
-
-// #define _GNU_SOURCE
-#define __EXPORTED_HEADERS__
-
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include <unistd.h>
-#include <string.h>
-#include <errno.h>
 #include <fcntl.h>
-#include <malloc.h>
-#include <stdbool.h>
-
 #include <sys/ioctl.h>
-#include <sys/syscall.h>
 #include <sys/mman.h>
 #include <linux/memfd.h>
 #include <linux/udmabuf.h>
-#include <cstdint>
 #include <memory>
-#include <iostream>
-#include <vector>
+#include "dma.h"
 
 #define CURRENT_RBP(x) \
   __asm__("mov %%rbp, %0;" \
@@ -37,7 +24,7 @@
 }
 
 
-uintptr_t virt_to_phys(void *virt)
+static uintptr_t virt_to_phys(void *virt)
 {
 	long pagesize = sysconf(_SC_PAGESIZE);
 	int fd = open("/proc/self/pagemap", O_RDONLY);
@@ -78,151 +65,95 @@ uintptr_t virt_to_phys(void *virt)
 	return (entry & 0x7fffffffffffffULL) * pagesize + ((uintptr_t)virt) % pagesize;
 }
 
-struct dma_mem{
-	void* virt;
-	long long phys;
-	size_t size;
-};
 
-class DmaAllocator{
-public:
-	DmaAllocator(){
-		devfd = open("/dev/udmabuf", O_RDWR);
-		if (devfd < 0)
-		{
-			printf("fail open\n");
-			exit(1);
-		}
-	}
-
-	~DmaAllocator(){
-		for(auto &a : buf_v) close(a);
-		for(auto &a : mem_v) munmap(a.virt, a.size);
-		for(auto &a : memfd_v) close(a);
-		close(devfd);
-	}
-
-	struct dma_mem alloc(off64_t sz){
-		struct udmabuf_create create = {0};
-		struct dma_mem mem = {0};
-
-		CHECK_NRVO("mem", mem);
-
-		page_size = getpagesize();
-		size = sz;	
-		create_memfd_with_seals(false);
-		mmap_fd();
-		write_to_memfd('X');
-
-		create.memfd = memfd;
-		create.offset = 0;
-		create.size = size;
-
-		buf = ioctl(devfd, UDMABUF_CREATE, &create);
-
-		if (buf < 0){
-			printf("fail %d\n", __LINE__);
-			exit(1);
-		}else{
-			printf("suceess %d\n", __LINE__);
-		}
-
-		buf_v.emplace_back(buf);
-
-		printf("phys addr: %lx\n", virt_to_phys(addr));
-
-		mem.size = sz;
-		mem.phys = virt_to_phys(addr);
-		mem.virt = addr;
-
-		mem_v.emplace_back(mem);
-
-		return mem;
-	}
-
-private:
-	void create_memfd_with_seals(bool hpage)
+DmaAllocator::DmaAllocator(){
+	devfd = open("/dev/udmabuf", O_RDWR);
+	if (devfd < 0)
 	{
-		int ret;
-		unsigned int flags = MFD_ALLOW_SEALING;
-
-		if (hpage)
-			flags |= MFD_HUGETLB;
-
-		memfd = memfd_create("udmabuf-test", flags);
-		if (memfd < 0)
-		{
-			printf("fail memfd_create\n");
-			exit(1);
-		}
-
-		memfd_v.emplace_back(memfd);
-
-		ret = fcntl(memfd, F_ADD_SEALS, F_SEAL_SHRINK);
-		if (ret < 0)
-		{
-			printf("fail fcntl\n");
-			exit(1);
-		}
-
-		ret = ftruncate(memfd, size);
-		if (ret == -1)
-		{
-			printf("fail ftruncate\n");
-			exit(1);
-		}
-
+		printf("fail open\n");
+		exit(1);
 	}
+}
 
-	void mmap_fd()
-	{
-		addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, memfd, 0);
-		if (addr == MAP_FAILED)
-		{
-			printf("fail mmap %d\n", __LINE__);
-			exit(1);
-		}
 
+DmaAllocator::~DmaAllocator(){
+	for(auto &a : buf_v) close(a);
+	for(auto &a : mem_v) munmap(a.virt, a.size);
+	for(auto &a : memfd_v) close(a);
+	close(devfd);
+}
+
+
+struct dma_mem DmaAllocator::alloc(off64_t sz){
+	struct udmabuf_create create = {0};
+	struct dma_mem mem = {0};
+	CHECK_NRVO("mem", mem);
+	page_size = getpagesize();
+	size = sz;	
+	create_memfd_with_seals(false);
+	mmap_fd();
+	write_to_memfd('X');
+	create.memfd = memfd;
+	create.offset = 0;
+	create.size = size;
+	buf = ioctl(devfd, UDMABUF_CREATE, &create);
+	if (buf < 0){
+		printf("fail %d\n", __LINE__);
+		exit(1);
+	}else{
+		printf("suceess %d\n", __LINE__);
 	}
+	buf_v.emplace_back(buf);
+	printf("phys addr: %lx\n", virt_to_phys(addr));
+	mem.size = sz;
+	mem.phys = virt_to_phys(addr);
+	mem.virt = addr;
+	mem_v.emplace_back(mem);
+	return mem;
+}
 
-	void write_to_memfd(char chr)
-	{
-		int i;
-
-		for (i = 0; i < size / page_size; i++)
-		{
-			*((char *)addr + (i * page_size)) = chr;
-		}
-	}
-
-
-private:
-	std::vector<struct dma_mem> mem_v;
-	std::vector<int> buf_v;
-	std::vector<int> memfd_v;
-
-	unsigned int page_size;
-	int memfd;
-	int devfd;
-	void *addr;
-	off64_t size;
-	int buf;
-
-};
-
-int main(int argc, char *argv[])
+void DmaAllocator::create_memfd_with_seals(bool hpage)
 {
-
+	int ret;
+	unsigned int flags = MFD_ALLOW_SEALING;
+	if (hpage)
+		flags |= MFD_HUGETLB;
+	memfd = memfd_create("udmabuf-test", flags);
+	if (memfd < 0)
 	{
-	std::unique_ptr<DmaAllocator> p = std::make_unique<DmaAllocator>();
-	struct dma_mem m1 = p->alloc(1024 * 1024);
-	struct dma_mem m2 = p->alloc(1024 * 1024);
-
-	printf("%llx\n", m1.phys); 
-
-	printf("%llx\n", m2.phys); 
+		printf("fail memfd_create\n");
+		exit(1);
 	}
-	sleep(5);
+	memfd_v.emplace_back(memfd);
+	ret = fcntl(memfd, F_ADD_SEALS, F_SEAL_SHRINK);
+	if (ret < 0)
+	{
+		printf("fail fcntl\n");
+		exit(1);
+	}
+	ret = ftruncate(memfd, size);
+	if (ret == -1)
+	{
+		printf("fail ftruncate\n");
+		exit(1);
+	}
+}
 
-	return 0;
+void DmaAllocator::mmap_fd()
+{
+	addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, memfd, 0);
+	if (addr == MAP_FAILED)
+	{
+		printf("fail mmap %d\n", __LINE__);
+		exit(1);
+	}
+}
+
+void DmaAllocator::write_to_memfd(char chr)
+{
+	int i;
+	for (i = 0; i < size / page_size; i++)
+	{
+		*((char *)addr + (i * page_size)) = chr;
+	}
 }
